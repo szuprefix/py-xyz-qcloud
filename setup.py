739bf27ce7cf102bd54681aae34aac49c20eae47
denishuang <szuprefix@126.com>
2019-12-11 17:27:19 +0800

    tron

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python  
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="xyz-qcloud",
+    version="0.0.0",
+    author="XYZSaas",
+    author_email="szuprefix@126.com",
+    description="qcloud utils",
+    long_description=open("README.rst").read(),
+    license="MIT",
+    url="https://github.com/szuprefix/py-xyz-qcloud",
+    packages=find_packages(exclude=['tests.*', 'tests', 'testproject', 'example.*', 'example']),
+    include_package_data=True,
+    install_requires=[
+        'django>=1.11.2',
+        'vod-python-sdk',
+        'tencentcloud-sdk-python'
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
---
+++ xyz_qcloud/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*- 
+# author = 'denishuang'
+from __future__ import unicode_literals
